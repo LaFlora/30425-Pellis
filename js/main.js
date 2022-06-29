5c4cc8129524ac001505ae26322aeb9cefad29e5
LaFlora <flora.pellis@gmail.com>
2022-06-29 18:43:49 -0300

    desafio complementario n°1

---
+++ js/main.js
@@ -0,0 +1,57 @@
+/*<script>
+    let nombre = prompt("ingrese su nombre");
+    let apellido = prompt("ingrese su apellido");
+    let edad = prompt ("ingrese su edad");
+
+    alert("Bienvenide " + nombre + "," + "tienes" + edad + " años verdad?");
+    alert("Has ingresado correctamente tus datos");
+</script>*/
+
+/*for(let i=0; i<=10; i++){
+    console.log(i);
+}*/
+
+//solicitar un valor al usuario
+/*let ingresarNumero=parseInt(prompt("Ingresar Número"));
+let resultado;
+for(let i=1; i<=10; i++){
+    resultado=ingresarNumero*i;
+}*/
+
+//orden de llegada de un grupo de maratonistas
+
+/*let repetir = true;
+let i=0;
+
+while (repetir){
+    i=i+1;
+    alert("¡Hola! Gracias por suscribirte");
+
+    if(i==5){
+        repetir=false;
+    }
+}*/
+
+let entrada="";
+
+while(entrada !="ESC"){
+    entrada=prompt("Ingrese su edad");
+    alert("Ingrese su verdadera edad");
+}
+
+/*let nota=1;
+
+switch(nota){
+    case 1:
+        alert("a marzo");
+        break;
+    case 2:
+        alert("a diciembre");
+        break;
+    case 3:
+        alert("aprueba");
+        break;
+    default:
+        alert("hay que estudiar mas");
+        break;
+}*/
