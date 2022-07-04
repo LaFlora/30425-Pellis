858eea3bf0b90073eeeb6d59d946f0ce273a0ad4
LaFlora <flora.pellis@gmail.com>
2022-07-03 23:46:13 -0300

    entrega del primer desafio obligatorio

--- js/main.js
+++ js/main.js
@@ -1,57 +1,42 @@
-/*<script>
-    let nombre = prompt("ingrese su nombre");
-    let apellido = prompt("ingrese su apellido");
-    let edad = prompt ("ingrese su edad");
+let carrito = 0;
+let inicio = Infinity;
 
-    alert("Bienvenide " + nombre + "," + "tienes" + edad + " años verdad?");
-    alert("Has ingresado correctamente tus datos");
-</script>*/
 
-/*for(let i=0; i<=10; i++){
-    console.log(i);
-}*/
+function registrarCurso() {
+  const ingles = 100;
+  const espanol = 200;
+  const frances = 250;
 
-//solicitar un valor al usuario
-/*let ingresarNumero=parseInt(prompt("Ingresar Número"));
-let resultado;
-for(let i=1; i<=10; i++){
-    resultado=ingresarNumero*i;
-}*/
-
-//orden de llegada de un grupo de maratonistas
-
-/*let repetir = true;
-let i=0;
-
-while (repetir){
-    i=i+1;
-    alert("¡Hola! Gracias por suscribirte");
-
-    if(i==5){
-        repetir=false;
+  for (let i = 0; i < inicio; i++) {
+    let curso = parseInt(
+      prompt(
+        "Elija el curso: \n 1- Ingles \n 2- Español \n 3-Frances \n 4- Salir"
+      )
+    );
+    while (isNaN(curso)) {
+      alert("Opcion incorrecta, vuelve a intentarlo");
+      curso = parseInt(
+        prompt(
+          "Elija el curso: \n 1- Ingles \n 2- Español \n 3-Frances \n 4- Salir"
+        )
+      );
     }
-}*/
-
-let entrada="";
-
-while(entrada !="ESC"){
-    entrada=prompt("Ingrese su edad");
-    alert("Ingrese su verdadera edad");
-}
-
-/*let nota=1;
-
-switch(nota){
-    case 1:
-        alert("a marzo");
+    if (curso == 1) {
+      carrito = carrito + ingles;
+      console.log(carrito);
+    } else if (curso == 2) {
+      carrito = carrito + espanol;
+      console.log(carrito);
+    } else if (curso == 3) {
+      carrito = carrito + frances;
+      console.log(carrito);
+    }
+    else{
+        console.log("TOTAL DE LA COMPRA= $" + carrito);
         break;
-    case 2:
-        alert("a diciembre");
-        break;
-    case 3:
-        alert("aprueba");
-        break;
-    default:
-        alert("hay que estudiar mas");
-        break;
-}*/
+    }
+  }
+};
+
+
+registrarCurso(inicio);
